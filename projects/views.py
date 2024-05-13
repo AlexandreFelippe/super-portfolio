30af259ddf726981aec8cf927cc7386e9f3b590d
AlexandreFelippe <alemfelippe@gmail.com>
2024-05-13 13:53:20 -0300

    Corrige requisito 03

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions
 from .models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import render
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -8,8 +10,18 @@ class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == 'GET':
+            profile = Profile.objects.get(pk=kwargs['pk'])
+            return render(request, 'profile_detail.html', {'profile': profile})
+        return super().retrieve(request, *args, **kwargs)
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
